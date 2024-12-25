e5d54b01025855630f9930f733304351a775bfed
Noidea22 <mathewsteenwinkel@gmail.com>
2024-12-25 11:19:10 -0700

    added vue project

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -2,12 +2,37 @@ import React from 'react'
 import './css/projects.css'
 import petals from './assets/GardenPage.png'
 import interview from './assets/235047457-8fa02a2b-0d32-4564-8506-700be825fd53.png'
+import VUEtodo from './assets/VUE TODO app preview.png'
+
 
 const Projects = () => {
   return (
+
+
+
     <div className='container'>
       <div className='projects'>
         <h1 className='title_proj'>Projects</h1>
+        <div className='container'>
+          <div className='container'>
+            <a href='https://github.com/mathewsteenwinkel/Vue-todo/tree/main/Todo' target='blank' className='contents'>
+              <div className='text3'>
+                <div className='titles'>
+                  <a className='proj_name'>VUE To Do</a>
+                  <div className='proj'>Vue Project</div>
+                </div>
+                <div className='info2'>
+                  <div className='year'>2024</div>
+                  <div className='stack'>VUE</div>
+                  <div className='stack2'>front end</div>
+                </div>
+              </div>
+              <img src={VUEtodo} className='img' />
+            </a>
+          </div>
+        </div>
+
+
         <div className='container'>
           <a href='https://github.com/mathewsteenwinkel/petal-profits' target='blank' className='contents'>
             <img src={petals} className='img' alt-text="petals" />
